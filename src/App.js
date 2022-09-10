68a214054aad83ef3543a3cca48a37dd2c7b59dd
GbadamosiMuibat <81063006+GbadamosiMuibat@users.noreply.github.com>
2022-09-10 23:39:49 +0100

    React Learning is fun

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ClassComponent from './components/ClassComponent';
+import FunctionalComponent from './components/FunctionalComponent';
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+       <div className="App">
+    <FunctionalComponent/>
+
+    <ClassComponent/>
+    </div>----
+    </Router>
+    
   );
 }
 
